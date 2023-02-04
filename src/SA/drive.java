a31eb7034b4df1fe7a95a07e212089403b3f6899
aihtnyc_h <feiver@naver.com>
2023-02-04 15:16:46 +0900

    2월 4일 항해99 12기 주특기 입문 주차 1일 과제

--- src/SA/drive.java
+++ src/SA/drive.java
@@ -4,7 +4,15 @@ public abstract class drive {
     //    void board(int pass, String dest, int dis) {
 //
 //    }
+    int maxPassenger;
+    //현재 승객수
+    int CurrentPassenger;
+    // 버스 비용
+    int cost; // 4월부터 1500원으로 인상
 
+    int busNumber;
+    // 현재 주유량
+    int currentGas;
     //    void board(int pass, String dest, int dis) {
 //
 //    }
